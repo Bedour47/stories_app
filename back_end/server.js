825068657213b37cb4b3b05e32a1fb48ae74a340
FaisalAlsharari <mr.fasoool.11@hotmail.com>
2019-09-05 12:29:00 +0300

    my changes

--- back_end/server.js
+++ back_end/server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
-
+const commentRoutes = require('./app/routes/comment_routes')
 // require error handling middleware
 const errorHandler = require('./lib/error_handler')
 
@@ -39,7 +39,9 @@ const app = express()
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`}))
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`
+}))
 
 // define port for API to run on
 const port = process.env.PORT || expressPort
@@ -57,12 +59,15 @@ app.use(auth)
 // The method `.use` sets up middleware for the Express application
 app.use(bodyParser.json())
 // this parses requests sent by `$.ajax`, which use a different content type
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // add request logger to create server log
 app.use(requestLogger)
 
 // register route files
+app.use(commentRoutes)
 app.use(exampleRoutes)
 app.use(userRoutes)
 
